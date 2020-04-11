68b01439296d5901c1777a07d8880fb3f72130c7
rogerbaba <rbarras@adon.li>
2020-04-11 22:45:37 +0200

    enhanched test script

--- Codes/testLexicon.py
+++ Codes/testLexicon.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from corpora_utils import CorporaHelper,CorporaDomains
 from senticnet_utils import SenticNetHelper
+from emotion_utils import Emotions, EmotionResult
+from emotion_analyzer import EmotionAnalyzer
+from random import random
 
 """
 corpora_helper = CorporaHelper("multigenre.csv")
@@ -13,8 +16,20 @@ print(blog_corpora)
 print('==============================')
 print('Start script output')
 print('==============================')
+concept = 'love_you'
+print('Test Senic Net with: ', concept)
 snh = SenticNetHelper()
-emotion = snh.get_emotion('love_you')
-
-print(emotion)
+emotion = snh.get_emotion(concept)
+print("SenticNet emotion: ",emotion)
+print('==============================')
+print("Mockup")
+analyzer = EmotionAnalyzer('love', mockup=True)
+print("Mockup emotionanalyzer: ",analyzer.get_emotion())
+print("primary emotion: ",EmotionResult.get_primary_emotion(analyzer.get_emotion()))
+print('==============================')
+corpus = 'love'
+print("Analyze: ", corpus)
+analyzer = EmotionAnalyzer(corpus, mockup=False)
+print("emotionanalyzer: ",analyzer.get_emotion())
+print("primary emotion: ",EmotionResult.get_primary_emotion(analyzer.get_emotion()))
 print('==============================')
