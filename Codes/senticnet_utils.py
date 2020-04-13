8193d04a1257bdeac1370c3ffec1608b8cb3749a
rogerbaba <rbarras@adon.li>
2020-04-13 21:25:11 +0200

    implement reduce emotion and negatate

--- Codes/senticnet_utils.py
+++ Codes/senticnet_utils.py
@@ -101,4 +101,162 @@ class SenticNetHelper():
 
         return emotion
 
+    @staticmethod
+    def reduce_emotion_to_senic_dimensions(emotion):
+        """
+        Reduces the emotion so it can have only one of the two opposite emotions
+            'pleasantness_value', 
+                => +:joy  -:sadness
+            'attention_value'
+                => +:anticipation  -:surprise
+            'sensitivity_value', 
+                => +:anger  -:fear
+            'aptitude_value'
+            => +:trust  -:disgust
+        """
+        reduced_emotion = emotion
+
+        # pleasantness_value
+        value = emotion[Emotions.JOY.value] - emotion[Emotions.SADNESS.value]
+        if value > 0:
+            reduced_emotion[Emotions.JOY.value] = value
+            reduced_emotion[Emotions.SADNESS.value] = 0
+        else:
+            reduced_emotion[Emotions.JOY.value] = 0
+            reduced_emotion[Emotions.SADNESS.value] = value
+
+        # attention_value
+        value = emotion[Emotions.ANTICIPATION.value] - emotion[Emotions.SURPRISE.value]
+        if value > 0:
+            reduced_emotion[Emotions.ANTICIPATION.value] = value
+            reduced_emotion[Emotions.SURPRISE.value] = 0
+        else:
+            reduced_emotion[Emotions.ANTICIPATION.value] = 0
+            reduced_emotion[Emotions.SURPRISE.value] = value
+
+        # sensitivity_value
+        value = emotion[Emotions.ANGER.value] - emotion[Emotions.FEAR.value]
+        if value > 0:
+            reduced_emotion[Emotions.ANGER.value] = value
+            reduced_emotion[Emotions.FEAR.value] = 0
+        else:
+            reduced_emotion[Emotions.ANGER.value] = 0
+            reduced_emotion[Emotions.FEAR.value] = value
+
+        # aptitude_value
+        value = emotion[Emotions.TRUST.value] - emotion[Emotions.DISGUST.value]
+        if value > 0:
+            reduced_emotion[Emotions.TRUST.value] = value
+            reduced_emotion[Emotions.DISGUST.value] = 0
+        else:
+            reduced_emotion[Emotions.TRUST.value] = 0
+            reduced_emotion[Emotions.DISGUST.value] = value      
+
+        return reduced_emotion
+        
+    @staticmethod
+    def negatate_emotion(emotion):
+        """
+        Negatate an emotion by a fixed value of 0.8 to the opposite emotion (1 is max value)
+        e.g. reduce anger:0.6 by 0.8 will become fear:0.2
+        or reduce anger:1 by 0.8 will become anger:0.2
+        Only negatate emotions with certain intesity to avoid strange results
+            => emotions above 0.4
+        """
+        min_value = 0.4
+        reduce_value = 0.8
+        temp_value = 0
+        
+        # negotation the emotion only can have one of the 4 emotion pleasentnes
+        neg_emotion = SenticNetHelper.reduce_emotion_to_senic_dimensions(emotion)
+        # ANGER - FEAR
+        if neg_emotion[Emotions.ANGER.value] > min_value:
+            # ANGER
+            temp_value = neg_emotion[Emotions.ANGER.value] - reduce_value
+            if temp_value < 0:
+                neg_emotion[Emotions.FEAR.value] = abs(temp_value)
+                neg_emotion[Emotions.ANGER.value] = 0
+            else:
+                neg_emotion[Emotions.ANGER.value] = abs(temp_value)
+        elif neg_emotion[Emotions.FEAR.value] > min_value:
+            # FEAR
+            temp_value = neg_emotion[Emotions.FEAR.value] - reduce_value
+            if temp_value < 0:
+                neg_emotion[Emotions.ANGER.value] = abs(temp_value)
+                neg_emotion[Emotions.FEAR.value] = 0
+            else:
+                neg_emotion[Emotions.FEAR.value] = abs(temp_value)
+        else :
+            # set to zero if no emotion is above min_value 
+            neg_emotion[Emotions.ANGER.value] = 0
+            neg_emotion[Emotions.FEAR.value] = 0
+
+        # JOY - SADNESS
+        if neg_emotion[Emotions.JOY.value] > min_value:
+            # JOY
+            temp_value = neg_emotion[Emotions.JOY.value] - reduce_value
+            if temp_value < 0:
+                neg_emotion[Emotions.SADNESS.value] = abs(temp_value)
+                neg_emotion[Emotions.JOY.value] = 0
+            else:
+                neg_emotion[Emotions.JOY.value] = abs(temp_value)
+        elif neg_emotion[Emotions.SADNESS.value] > min_value:
+            # SADNESS
+            temp_value = neg_emotion[Emotions.SADNESS.value] - reduce_value
+            if temp_value < 0:
+                neg_emotion[Emotions.JOY.value] = abs(temp_value)
+                neg_emotion[Emotions.SADNESS.value] = 0
+            else:
+                neg_emotion[Emotions.SADNESS.value] = abs(temp_value)
+        else :
+            # set to zero if no emotion is above min_value 
+            neg_emotion[Emotions.JOY.value] = 0
+            neg_emotion[Emotions.SADNESS.value] = 0
+
+        # ANICIPATION - SURPRISE
+        if neg_emotion[Emotions.ANTICIPATION.value] > min_value:
+            # ANICIPATION
+            temp_value = neg_emotion[Emotions.ANTICIPATION.value] - reduce_value
+            if temp_value < 0:
+                neg_emotion[Emotions.SURPRISE.value] = abs(temp_value)
+                neg_emotion[Emotions.ANTICIPATION.value] = 0
+            else:
+                neg_emotion[Emotions.ANTICIPATION.value] = abs(temp_value)
+        elif neg_emotion[Emotions.SURPRISE.value] > min_value:
+            # SURPRISE
+            temp_value = neg_emotion[Emotions.SURPRISE.value] - reduce_value
+            if temp_value < 0:
+                neg_emotion[Emotions.ANTICIPATION.value] = abs(temp_value)
+                neg_emotion[Emotions.SURPRISE.value] = 0
+            else:
+                neg_emotion[Emotions.SURPRISE.value] = abs(temp_value)
+        else :
+            # set to zero if no emotion is above min_value 
+            neg_emotion[Emotions.ANTICIPATION.value] = 0
+            neg_emotion[Emotions.SURPRISE.value] = 0
+
+        # TRUST - DISGUST
+        if neg_emotion[Emotions.TRUST.value] > min_value:
+            # TRUST
+            temp_value = neg_emotion[Emotions.TRUST.value] - reduce_value
+            if temp_value < 0:
+                neg_emotion[Emotions.DISGUST.value] = abs(temp_value)
+                neg_emotion[Emotions.TRUST.value] = 0
+            else:
+                neg_emotion[Emotions.TRUST.value] = abs(temp_value)
+        elif neg_emotion[Emotions.DISGUST.value] > min_value:
+            # DISGUST
+            temp_value = neg_emotion[Emotions.DISGUST.value] - reduce_value
+            if temp_value < 0:
+                neg_emotion[Emotions.TRUST.value] = abs(temp_value)
+                neg_emotion[Emotions.DISGUST.value] = 0
+            else:
+                neg_emotion[Emotions.DISGUST.value] = abs(temp_value)
+        else :
+            # set to zero if no emotion is above min_value 
+            neg_emotion[Emotions.TRUST.value] = 0
+            neg_emotion[Emotions.DISGUST.value] = 0
+
+        return neg_emotion
+
 
