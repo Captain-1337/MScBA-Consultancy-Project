a18efe949da5e87370f942f97ce135b8e903c833
rogerbaba <rbarras@adon.li>
2020-06-30 21:00:05 +0200

    add fold runs and data enrichment sav and load

--- Codes/testDeep1MulticlassKfold.py
+++ Codes/testDeep1MulticlassKfold.py
@@ -2,6 +2,7 @@ from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
 from keras.layers import Embedding, Flatten, Dense, Conv1D
+from keras import layers
 from sklearn.preprocessing import scale
 from sklearn.model_selection import KFold, StratifiedKFold
 from corpora_utils import CorporaHelper,CorporaDomains, CorporaProperties
@@ -12,22 +13,34 @@ import pickle
 Binary simple NN with two layers and 4 emotions
 """
 
-#corpus = 'All this not sleeping has a terrible way of playing with your memory.' # fear => test
-#corpus = "The Rock is destined to be the 21st Century s new Conan and that he s going to make a splash even greater than Arnold Schwarzenegger , Jean-Claud Van Damme or Steven Segal ."
-corpus = 'If you sometimes like to go to the movies to have fun , Wasabi is a good place to start .' # joy 
 # load data
 labels = []
 texts = []
 corpora_helper = CorporaHelper("multigenre.csv")
+# enrich some emotions with random copies to raise the number of samples
+# anger from 240 to 400 => add 160 random
+corpora_helper.random_enrich_emotion('anger', 160)
+# fear from 263 to 400 => add 137 random
+corpora_helper.random_enrich_emotion('fear', 137)
 count_joy = 0
 count_sadness = 0
 count_anger = 0
 count_fear = 0
-number_of_classes: 4
-max_per_emotion = 240
-max_data = 4*240
+#number_of_classes = 4
+max_per_emotion = 400
 
-
+# K-Fold variables
+num_folds = 10
+fold_runs = 3
+fold_no = 1
+# trian
+epochs = 10
+skfold = StratifiedKFold(n_splits = num_folds, random_state = 7, shuffle = True)
+acc_per_fold = []
+loss_per_fold = []
+avg_acc_per_run = []
+avg_loss_per_run = []
+create_final_model = True
 
 # preprocessing corpora
 corpora_helper.translate_contractions() # problem space before '
@@ -44,8 +57,7 @@ corpora_helper.add_space_at_special_chars()
 for index, corpus in corpora_helper.get_data().iterrows():
     # only moviereviews
     if True or corpus[CorporaProperties.DOMAIN.value] == CorporaDomains.MOVIEREVIEW.value:
-        # only joy
-        # only disgust
+        # only anger/ fear / joy / sadness
         if corpus[CorporaProperties.EMOTION.value] == 'anger':
             if max_per_emotion > count_anger:
                 texts.append(corpus[CorporaProperties.CLEANED_CORPUS.value])
@@ -75,13 +87,7 @@ max_data = count_anger + count_fear + count_joy + count_sadness
 # 1 fear
 # 2 joy
 # 3 sadness
-
-# K-Fold variables
-num_folds = 5
-fold_no = 1
-skfold = StratifiedKFold(n_splits = num_folds, random_state = 7, shuffle = True)
-acc_per_fold = []
-loss_per_fold = []
+#maxlen = 100 # max. number of words in sequences
 
 
 ## Create one hot encoding
@@ -119,9 +125,9 @@ for word, i in word_index.items():
             # Words not found in embedding index will be all-zeros.
             word_embedding_matrix[i] = embedding_vector
 embedding = Embedding(max_words, embedding_dim, input_length=maxlen)
-"""
 
-# Load prepared Multigenre embedding
+"""
+# Load prepared Multigenre ensemble embedding
 
 word_embeddings_path = 'multigenre_embedding_final_new.pkl'
 with open(word_embeddings_path, 'rb') as word_embeddings_file:
@@ -163,8 +169,6 @@ maxlen = MAX_SEQUENCE_LENGTH
 EMBEDDING_DIM = len(get_unigram_embedding("glad", embedding_info[0], unigram_feature_string))
 print("Embedding dimension:",EMBEDDING_DIM)
 
-
-
 # Matrix
 word_embedding_matrix = list()
 word_embedding_matrix = np.zeros((max_words, EMBEDDING_DIM)) # evtl. change to word_indices_len
@@ -190,9 +194,9 @@ embedding = Embedding(max_words, EMBEDDING_DIM, input_length=maxlen, trainable=F
 # Prepare data
 #maxlen = 100 # max. number of words in sequences
 
-training_samples = int(max_data * 0.9) #672  70% of 960
-validation_samples = int(max_data * 0) # 192 20% of 960
-test_samples = int(max_data * 0.1) #96  10% of 960
+training_samples = int(max_data * 0.8) 
+validation_samples = int(max_data * 0) 
+test_samples = int(max_data * 0.2) 
 
 data = pad_sequences(sequences, maxlen=maxlen)
 
@@ -214,17 +218,118 @@ y_val = labels[training_samples: training_samples + validation_samples]
 x_test = data[training_samples + validation_samples: training_samples + validation_samples + test_samples]
 y_test = labels[training_samples + validation_samples: training_samples + validation_samples + test_samples]
 
+# Store x,y train and test 8 2
+with open('multigenre_400_train_corpus.pkl', 'wb') as writer:
+    pickle.dump(x_train, writer)
+with open('multigenre_400_train_labels.pkl', 'wb') as writer:
+    pickle.dump(y_train, writer)
+with open('multigenre_400_test_corpus.pkl', 'wb') as writer:
+    pickle.dump(x_test, writer)
+with open('multigenre_400_test_labels.pkl', 'wb') as writer:
+    pickle.dump(y_test, writer)
 
-for train_ind, val_ind in skfold.split(x_train,y_train):
 
+# Load x,y test
+with open('multigenre_400_train_corpus.pkl', 'rb') as loader:
+    x_train = pickle.load(loader)
+with open('multigenre_400_train_labels.pkl', 'rb') as loader:
+    y_train = pickle.load(loader)
+with open('multigenre_400_test_corpus.pkl', 'rb') as loader:
+    x_test = pickle.load(loader)
+with open('multigenre_400_test_labels.pkl', 'rb') as loader:
+    y_test = pickle.load(loader)
+
+def create_model():
     # Create model
+    """
     model = Sequential()
     model.add(embedding)
     model.add(Conv1D(32,5, activation='relu'))
     model.add(Flatten()) #3D to 2D
-    model.add(Dense(32, activation='relu'))
+    #model.add(Dense(32, activation='relu'))
     model.add(Dense(4, activation='softmax'))
     #model.summary()
+"""
+    model = Sequential()
+    model.add(embedding)
+    model.add(Conv1D(32,5, activation='relu'))
+    model.add(layers.Bidirectional(layers.LSTM(32,dropout=0.4, recurrent_dropout=0.4,)))
+    #model.add(Dense(8, activation='relu'))
+    #model.add(Dense(32, activation='relu'))
+    model.add(Dense(4, activation='softmax'))
+
+    return model
+# run x Times
+for run_num in range(1,fold_runs+1):
+    # k-fold
+    for train_ind, val_ind in skfold.split(x_train,y_train):
+
+        # Create model
+        model = create_model()
+
+        # Load GloVe embedding
+        model.layers[0].set_weights([word_embedding_matrix])
+        model.layers[0].trainable = False
+
+        # Train and Evaluate
+        model.compile(optimizer='adam',
+                    loss='sparse_categorical_crossentropy',
+                    metrics=['acc'])
+        print('------------------------------------------------------------------------')
+        print(f'Training for fold {fold_no} ind run {run_num} ...')
+
+        history = model.fit(x_train[train_ind], y_train[train_ind],
+                            epochs=epochs,
+                            batch_size=32,
+                            verbose=1,
+                            validation_data=(x_train[val_ind], y_train[val_ind]))
+
+        # metrics
+        scores = model.evaluate(x_train[val_ind], y_train[val_ind], batch_size=128)
+        #print(f'Score for fold {fold_no}: {model.metrics_name[0]} of {scores[0]}; {model.metrics_name[1]} of {scores[1]*100}%')
+        print(f'Score for fold {fold_no}: ... of {scores[0]}; ... of {scores[1]*100}%')
+        acc_per_fold.append(scores[1]*100)
+        loss_per_fold.append(scores[0])
+
+        fold_no += 1
+
+
+    # == Provide average scores ==
+    print('------------------------------------------------------------------------')
+    print('Score per fold')
+    for i in range(0, len(acc_per_fold)):
+        print('------------------------------------------------------------------------')
+        print(f'> Fold {i+1} - Loss: {loss_per_fold[i]} - Accuracy: {acc_per_fold[i]}%')
+    print('------------------------------------------------------------------------')
+    print('Average scores for all folds:')
+    avg_acc_per_run.append(np.mean(acc_per_fold))
+    avg_loss_per_run.append(np.mean(loss_per_fold))
+    print(f'> Accuracy: {np.mean(acc_per_fold)} (+- {np.std(acc_per_fold)})')
+    print(f'> Loss: {np.mean(loss_per_fold)}')
+    print('------------------------------------------------------------------------')
+
+    # reset fold vars
+    acc_per_fold = []
+    loss_per_fold = []
+    fold_no = 1
+
+# == Provide average scores ==
+print('------------------------------------------------------------------------')
+print('Score per fold')
+for i in range(0, len(avg_acc_per_run)):
+    print('------------------------------------------------------------------------')
+    print(f'> Run {i+1} Fold averages - Loss: {avg_loss_per_run[i]} - Accuracy: {avg_acc_per_run[i]}%')
+print('------------------------------------------------------------------------')
+print(f'Overall average scores for all {fold_runs} runs:')
+
+print(f'> Accuracy: {np.mean(avg_acc_per_run)} (+- {np.std(avg_acc_per_run)})')
+print(f'> Loss: {np.mean(avg_loss_per_run)}')
+print('------------------------------------------------------------------------')
+
+# create final model #Todo sync with fold rund
+if create_final_model:
+    model = create_model()
+    model.summary()
 
     # Load GloVe embedding
     model.layers[0].set_weights([word_embedding_matrix])
@@ -235,69 +340,44 @@ for train_ind, val_ind in skfold.split(x_train,y_train):
                 loss='sparse_categorical_crossentropy',
                 metrics=['acc'])
     print('------------------------------------------------------------------------')
-    print(f'Training for fold {fold_no} ...')
+    print('Training for final model ...')
 
     history = model.fit(x_train[train_ind], y_train[train_ind],
-                        epochs=10,
+                        epochs=epochs,
                         batch_size=32,
-                        verbose=1,
-                        validation_data=(x_train[val_ind], y_train[val_ind]))
+                        verbose=1)
+    model.save('model_emotion_detection.h5')   
 
-    # metrics
-    scores = model.evaluate(x_train[val_ind], y_train[val_ind], batch_size=128)
-    #print(f'Score for fold {fold_no}: {model.metrics_name[0]} of {scores[0]}; {model.metrics_name[1]} of {scores[1]*100}%')
-    print(f'Score for fold {fold_no}: ... of {scores[0]}; ... of {scores[1]*100}%')
-    acc_per_fold.append(scores[1]*100)
-    loss_per_fold.append(scores[0])
-
-    fold_no += 1
+    # Test final model
+    print("Evaluate Findal Model on test data")
+    results = model.evaluate(x_test, y_test, batch_size=128)
+    print("test loss, test acc:", results)
 
 
-# == Provide average scores ==
-print('------------------------------------------------------------------------')
-print('Score per fold')
-for i in range(0, len(acc_per_fold)):
-  print('------------------------------------------------------------------------')
-  print(f'> Fold {i+1} - Loss: {loss_per_fold[i]} - Accuracy: {acc_per_fold[i]}%')
-print('------------------------------------------------------------------------')
-print('Average scores for all folds:')
-print(f'> Accuracy: {np.mean(acc_per_fold)} (+- {np.std(acc_per_fold)})')
-print(f'> Loss: {np.mean(loss_per_fold)}')
-print('------------------------------------------------------------------------')
+    # Plot performance
+    """"
+    import matplotlib.pyplot as plt
 
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
 
-# Test final model
-"""
-print("Evaluate on test data")
-model.load_weights('pre_trained_glove_model.h5')
-results = model.evaluate(x_test, y_test, batch_size=128)
-print("test loss, test acc:", results)
-#model.save('emotion_deep1.h5')
-"""
-# Plot performance
-"""
-import matplotlib.pyplot as plt
+    epochs = range(1, len(acc) + 1)
 
-acc = history.history['acc']
-val_acc = history.history['val_acc']
-loss = history.history['loss']
-val_loss = history.history['val_loss']
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
 
-epochs = range(1, len(acc) + 1)
+    plt.figure()
 
-plt.plot(epochs, acc, 'bo', label='Training acc')
-plt.plot(epochs, val_acc, 'b', label='Validation acc')
-plt.title('Training and validation accuracy')
-plt.legend()
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
 
-plt.figure()
-
-plt.plot(epochs, loss, 'bo', label='Training loss')
-plt.plot(epochs, val_loss, 'b', label='Validation loss')
-plt.title('Training and validation loss')
-plt.legend()
-
-plt.show()
+    plt.show()
 """
 
 
