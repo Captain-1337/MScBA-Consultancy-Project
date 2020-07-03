e87553caa5ce6f7675bb47e07e4bf41f02a30777
rogerbaba <rbarras@adon.li>
2020-07-03 10:43:37 +0200

    splitting data scripts

--- Codes/splitTrainTestTwitter.py
+++ Codes/splitTrainTestTwitter.py
@@ -2,17 +2,19 @@ from corpora_utils import CorporaHelper,CorporaDomains, CorporaProperties
 import numpy as np
 import os
 """
-Split multigenre data in Train and Test set
+Split twitter data in Train and Test set
 """
 
-corpora_helper = CorporaHelper("multigenre.csv")
+corpora_helper = CorporaHelper("emoint_all_emotions.csv", separator='\t')
 corpora_helper.remove_cleaned_corpus()
+corpora_helper.remove_column('intensity')
+corpora_helper.add_domain('twitter')
 
 count_joy = 0
 count_sadness = 0
 count_anger = 0
 count_fear = 0
-max_per_emotion = 450
+max_per_emotion = 2000
 
 corpora_helper.remove_emotion("other")
 corpora_helper.remove_emotion("noemotion")
@@ -39,8 +41,8 @@ train, test = corpora_helper.split_train_test_data(0.9)
 # Train/validate Dataset
 train_corpora_helper = CorporaHelper()
 train_corpora_helper.set_data(train)
-trainfilepath = 'corpora/multigenre_450_train.csv'
-train_corpora_helper.write_to_csv(trainfilepath,sep=';')
+trainfilepath = 'corpora/twitter_2000_train.csv'
+train_corpora_helper.write_to_csv(trainfilepath,sep='\t')
 print('number of anger labels train: ', train_corpora_helper.get_emotion_data('anger').shape[0])
 print('number of fear labels train: ', train_corpora_helper.get_emotion_data('fear').shape[0])
 print('number of joy labels train: ', train_corpora_helper.get_emotion_data('joy').shape[0])
@@ -51,8 +53,8 @@ print(f'Train dataset has been stored to: {trainfilepath}')
 # Test Data set
 test_corpora_helper = CorporaHelper()
 test_corpora_helper.set_data(test)
-testfilepath = 'corpora/multigenre_450_test.csv'
-test_corpora_helper.write_to_csv(testfilepath,sep=';')
+testfilepath = 'corpora/twitter_2000_test.csv'
+test_corpora_helper.write_to_csv(testfilepath,sep='\t')
 print('number of anger labels test: ', test_corpora_helper.get_emotion_data('anger').shape[0])
 print('number of fear labels test: ', test_corpora_helper.get_emotion_data('fear').shape[0])
 print('number of joy labels test: ', test_corpora_helper.get_emotion_data('joy').shape[0])
